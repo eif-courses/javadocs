adb6113816396b27abbcb67db6c541f36c1858d9
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2021-06-26 17:32:55 +0300

    aa

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  docs: {
+    primaryColor: '#E24F55'
+  },
+  i18n: {
+    locales: () => [{
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'en'
+  },
+})
